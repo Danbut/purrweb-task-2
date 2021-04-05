9ff289412f5a1e12951b17323abfe30d1d839d51
Restyled.io <commits@restyled.io>
2021-04-05 12:14:53 +0000

    Restyled by clang-format

--- metro.config.js
+++ metro.config.js
@@ -2,15 +2,16 @@ const {getDefaultConfig} = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: {sourceExts, assetExts},
-  } = await getDefaultConfig();
+    resolver : {sourceExts, assetExts},
+  } =
+      await getDefaultConfig();
   return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    transformer : {
+      babelTransformerPath : require.resolve('react-native-svg-transformer'),
     },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
+    resolver : {
+      assetExts : assetExts.filter(ext => ext !== 'svg'),
+      sourceExts : [...sourceExts, 'svg' ],
     },
   };
 })();
